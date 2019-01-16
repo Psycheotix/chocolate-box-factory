1b64871b67d82bb83ed5a821ea8ae88397aed5fb
CADeer <christinedeer@gmail.com>
2019-01-16 17:29:21 -0500

    added some css to increase the cuteness

--- src/components/InfoBar/index.js
+++ src/components/InfoBar/index.js
@@ -4,7 +4,11 @@ import "./style.css";
 function InfoBar(props) {
   return (
   <div className="infoBar">
-  <div className="receipt-play">How to play</div>
+    <div className="receipt-play">
+      <p>
+       How to play
+      </p>
+    </div>
   <div className="receipt-score"> Score {props.score}</div></div>
   );
 }
